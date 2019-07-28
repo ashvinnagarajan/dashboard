d049a02d5cc2b897b886da3f8cf70d7a80835ee5
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-07-28 16:35:51 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import DoughnutGraph from './doughnut';
+import PieGraph from './pie';
+import LineGraph from './line';
+// import BarExample from './bar';
+// import HorizontalBarExample from './horizontalBar';
+// import RadarExample from './radar';
+// import PolarExample from './polar';
+// import BubbleExample from './bubble';
+// import ScatterExample from './scatter';
+// import MixedDataExample from './mix';
+// import RandomizedDataLineExample from './randomizedLine';
+// import CrazyDataLineExample from './crazyLine';
+// import LegendOptionsExample from './legend-options';
+// import LegendHandlersExample from './legend-handlers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+				<hr />
+				<DoughnutGraph />
+				<hr />
+				<PieGraph />
+        <hr />
+        <LineGraph />
+    
     </div>
+    </div>
+
+    
   );
 }
 
