6b5551c8dcc0af0ed442242aab7eeaae2fc69e23
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-08-18 21:54:52 -0700

    Updates

---
+++ precache-manifest.ae009a62df31ae0e300d1cbf0801bf7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1be6f8623972e82dedcbafbc8c242b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6001a28f844f07529613",
+    "url": "/static/css/main.3986cfeb.chunk.css"
+  },
+  {
+    "revision": "6c31f9fe1c61a305a995",
+    "url": "/static/js/2.9c22f5a8.chunk.js"
+  },
+  {
+    "revision": "6001a28f844f07529613",
+    "url": "/static/js/main.2c3a1ea4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
