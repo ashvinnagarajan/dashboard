cdd910938e0e27a8375c4f02c204edb8452f3db7
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-08-18 23:52:22 -0700

    hid the apiKey as I should have

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
  import firebase from 'firebase';
+ import {apiKeyHidden} from './config';
  
  // Set the configuration for your app
 // TODO: Replace with your project's config object
 
   var config = {
-    apiKey: "AIzaSyAnFTWZTgWEugKOSUM6WY_NkxrPzRzn6dU",
+    apiKey: apiKeyHidden,
     authDomain: "bruin-racing.firebaseapp.com",
     databaseURL: "https://bruin-racing.firebaseio.com",
     projectId: "bruin-racing",
