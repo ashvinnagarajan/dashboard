8ec048b5a4b29ca58a5ce5b37bdf9c43f01b228f
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-09-05 22:01:14 -0700

    Fixed metric for efficiency

--- src/components/joulemeter.js
+++ src/components/joulemeter.js
@@ -8,9 +8,9 @@ class Joulemeter extends React.Component{
                <div className="card-content">
                     <p className="title is-5">Joulemeter</p>
                     <p className="subtitle is-6">{this.props.joulemeter.volt} V | {this.props.joulemeter.amp} A</p>
-                    <p className="subtitle is-6">Instant Efficiency: {this.props.joulemeter.instant} W</p>
-                    <p className="subtitle is-6">Avg Efficiency: {this.props.joulemeter.avg} W</p>
-                    <p className="subtitle is-6">Peak Efficiency: {this.props.joulemeter.peak} W</p>
+                    <p className="subtitle is-6">Instant Efficiency: {this.props.joulemeter.instant} mi/kWh</p>
+                    <p className="subtitle is-6">Avg Efficiency: {this.props.joulemeter.avg} mi/kWh</p>
+                    <p className="subtitle is-6">Peak Efficiency: {this.props.joulemeter.peak} mi/kWh</p>
                </div>
           </div>
         );
