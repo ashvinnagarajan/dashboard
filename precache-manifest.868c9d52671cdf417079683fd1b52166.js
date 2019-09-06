6f451802fb1ee74c1c8405b9e08e43dc0bb1f18c
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-09-05 22:08:22 -0700

    Updates

--- precache-manifest.868c9d52671cdf417079683fd1b52166.js
+++ precache-manifest.868c9d52671cdf417079683fd1b52166.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf2a7c332a22fbe90d74b1a5b5538732",
+    "revision": "b602a05634e92daaa9281bdbe8db9f42",
     "url": "/dashboard/index.html"
   },
   {
-    "revision": "7dafa324282bd5fe8c5f",
+    "revision": "e35fd7268a31bf150ac5",
     "url": "/dashboard/static/css/main.9d94ae1a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dashboard/static/js/2.108d8b68.chunk.js"
   },
   {
-    "revision": "7dafa324282bd5fe8c5f",
-    "url": "/dashboard/static/js/main.9a6605b3.chunk.js"
+    "revision": "e35fd7268a31bf150ac5",
+    "url": "/dashboard/static/js/main.0387b859.chunk.js"
   },
   {
     "revision": "dcfbc05562333eb34d98",
