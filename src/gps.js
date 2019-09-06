df6791022e92ab2324a9993ae85842a9c017c7f6
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-09-05 17:11:04 -0700

    Update gps.js

--- src/gps.js
+++ src/gps.js
@@ -4,8 +4,8 @@ class Weather extends React.Component{
 
     render() {
         return (
-          <div className="weather">
-               <div className="card-content">
+          <div className="GPS">
+               <div className="card-content GPS">
                     <p className="subtitle is-6">Latitude: 45.1231</p>
                     <p className="subtitle is-6">Longitude: 10.1223</p>
                </div>
