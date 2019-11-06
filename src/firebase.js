733a105fdb0280084bdad0647beb4f2ac106a990
ashvinnagarajan <50090261+ashvinnagarajan@users.noreply.github.com>
2019-11-06 11:36:36 -0800

    fixed apikey hidden

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
- import firebase from 'firebase';
- import {apiKeyHidden} from './config';
- 
- // Set the configuration for your app
+import apiKeyHidden from './config';
+import firebase from 'firebase';
+
+// Set the configuration for your app
 // TODO: Replace with your project's config object
 
-  var config = {
-    apiKey: apiKeyHidden,
-    authDomain: "bruin-racing.firebaseapp.com",
-    databaseURL: "https://bruin-racing.firebaseio.com",
-    projectId: "bruin-racing",
-    storageBucket: "bucket.appspot.com"
-  };
+var config = {
+  // apiKey: "AIzaSyAnFTWZTgWEugKOSUM6WY_NkxrPzRzn6dU",
+  apiKey: apiKeyHidden,
+  authDomain: "bruin-racing.firebaseapp.com",
+  databaseURL: "https://bruin-racing.firebaseio.com",
+  projectId: "bruin-racing",
+  storageBucket: "bucket.appspot.com"
+};
 
-  firebase.initializeApp(config);
-  
-  export default firebase;
+firebase.initializeApp(config);
+
+export default firebase;
